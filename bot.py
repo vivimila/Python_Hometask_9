f8559df6801309aca730a735806b89fc835432c9
vivimila <violamilashevskaya@yandex.by>
2022-12-08 21:47:24 +0300

    Work with date

--- bot.py
+++ bot.py
@@ -10,12 +10,15 @@ TOKEN = "5864815319:AAGLJHzbAUIVVHWCRu1rRDCGdBp3lE6NVXY"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
-@dp.message_handler(commands=["start", "help"])
-async def start(message: types.Message):
-    await message.answer("Hi!")
+@dp.message_handler(commands=['start'])
+def start_hendler(message: types.Message):
+    user_id = message.from_user.id
+    user_full_name = message.from_user.full_name
+    logging.INFO(f'{user_id} {user_full_name}', time.asctime()) 
 
-@dp.message_handler(commands=["new_chat_members"])
-async def start(message: types.Message):
-    await message.answer("Добро пожаловать!")    
+
+#@dp.message_handler(commands=["new_chat_members"])
+#async def start(message: types.Message):
+    #await message.answer("Добро пожаловать!")    
 
     
