dc267b9c2c8b87cc5776211f53659a81d4287168
vivimila <violamilashevskaya@yandex.by>
2022-12-08 22:00:48 +0300

    Try to run the programm

--- bot.py
+++ bot.py
@@ -7,14 +7,23 @@ from aiogram import Bot, types, executor, Dispatcher
 
 TOKEN = "5864815319:AAGLJHzbAUIVVHWCRu1rRDCGdBp3lE6NVXY"
 
+MSG = "Мы давно не виделись!)"
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
-def start_hendler(message: types.Message):
+async def start_hendler(message: types.Message):
     user_id = message.from_user.id
-    user_full_name = message.from_user.full_name
-    logging.INFO(f'{user_id} {user_full_name}', time.asctime()) 
+    user_name = message.from_user.full_name
+    user_full_name = message.from_user.first_name
+    logging.info(f'{user_id} {user_full_name} {time.asctime()}')   
+    await message.reply(f'Привет, {user_full_name}!')
+
+    for i in range(10):
+        time.sleep(2)
+
+        await bot.send_message(user_id, MSG.format(user_name))
 
 
 #@dp.message_handler(commands=["new_chat_members"])
@@ -22,3 +31,5 @@ def start_hendler(message: types.Message):
     #await message.answer("Добро пожаловать!")    
 
     
+if __name__ == '__main__':
+    executor.start_polling(dp)
